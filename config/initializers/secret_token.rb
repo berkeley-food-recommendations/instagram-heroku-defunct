bcec659239b9bf71f9af39094d7acab2457b4742
Helen <helen@helenweng.com>
2012-10-28 20:33:50 -0700

    initial rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstagramHeroku::Application.config.secret_token = 'a6c6b0aa92effe2d9021b69fd2b29e81550338c4d1e786a6b075bd297329289cc549d3f88bd13f5cfe769e3d63480074ea838ad7625d7e16d16bb979604f2660'
